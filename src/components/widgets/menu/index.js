a65dedd3af0eb357dc5b8008498e98c2d7bdef7c
root <jorge.reales4@gmail.com>
2021-05-10 21:01:32 -0500

    first commit

--- src/components/widgets/menu/index.js
+++ src/components/widgets/menu/index.js
@@ -27,11 +27,11 @@ function Menu(props){
     <Nav className="mr-auto">
       <Nav.Link href="https://master.d2p0wcikpkc50a.amplifyapp.com" style={menuSelected} >Inicio</Nav.Link>
       <NavDropdown style={menuStyle} title="Nuestros Servicios" id="collasible-nav-dropdown" >
-        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Terapia de Pareja y Familiar</NavDropdown.Item>
-        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Terapia Psicológica de Niños, Niñas, y Adolescentes.</NavDropdown.Item>
-        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Atención Integral para la Vejez</NavDropdown.Item>
-        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Terapia Sexual</NavDropdown.Item>
-        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Apoyo en el Proceso de Elaboración de duelo</NavDropdown.Item>
+        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Servicio 1</NavDropdown.Item>
+        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Servicio 2</NavDropdown.Item>
+        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Servicio 3</NavDropdown.Item>
+        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Servicio 4</NavDropdown.Item>
+        <NavDropdown.Item href="https://master.d2p0wcikpkc50a.amplifyapp.com/servicio" style={menuStyle}>Servicio 5</NavDropdown.Item>
       </NavDropdown>
       <Nav.Link href="#link" style={menuStyle} >Quienes Somos</Nav.Link>
       <Nav.Link href="#link" style={menuStyle} >Contacto</Nav.Link>
