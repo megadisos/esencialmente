a87ff75dd465b4a47f4b5a19c2c56a1eb3a2eba0
root <jorge.reales4@gmail.com>
2021-05-15 20:10:34 -0500

    api

--- src/components/widgets/footer/index.js
+++ src/components/widgets/footer/index.js
@@ -1,17 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState,useContext} from 'react'
 import {Row, Col} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPhone, faEnvelope, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons'
+import {mycontext} from './../../../App'
 function Footer(props){
     let iconStyle = {
         "color":"#1c8e96",
     }
+const {informacion,servicios} = useContext(mycontext);
+const [info] = informacion;
+const [serv] = servicios;
  return (
      
          <Row className="footer mt-5" >
-             <Col md={4} className="mt-2"><h6>Nuestros servicios</h6></Col>
-             <Col  md={4} className="mt-2"><h6>Contactanos</h6></Col>
-             <Col  md={4} className="mt-2"><h6>Siguenos en redes</h6></Col>
+             <Col md={4} className="mt-2">
+                 <h6>Nuestros servicios</h6>
+                 {serv && serv.map(sr=>{
+                     return(
+                        <p>{serv.titulo}</p>
+                     )
+                 })}
+                 </Col>
+             <Col  md={4} className="mt-2">
+                 <h6>Contactanos</h6>
+                 <p>{info[0] && info[0].numero}</p>
+                 <p>{info[0] && info[0].correo}</p>
+                 <p>{info[0] && info[0].direccion}</p>
+                 </Col>
+             <Col  md={4} className="mt-2">
+                 <h6>Siguenos en redes</h6>
+                 <p><a href={info[0] && info[0].facebook}>facebook</a></p>
+                 <p><a href={info[0] && info[0].instagram}>instagram</a></p>
+                 <p><a href={info[0] && info[0].twitter}>twitter</a></p>
+                 </Col>
          </Row>
  )
 }
