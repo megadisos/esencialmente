d472832205bf158c8f866bc924fdbc84251cf85a
root <jorge.reales4@gmail.com>
2021-05-15 19:58:58 -0500

    api

--- src/components/widgets/cafe/index.js
+++ src/components/widgets/cafe/index.js
@@ -1,16 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState,useContext} from 'react'
 import {Image, Row, Container,Col} from 'react-bootstrap'
 import Servicio from '../servicio'
 import Estacion from './../../../images/estacion.jpg'
+import {mycontext} from './../../../App'
 function Cafe(props){
+    const {invitacion} = useContext(mycontext);
+    const [inv] = invitacion;
     return(
         <Container className="invitacion mt-5" fluid>
       
       <Row > 
-      <Col md={6} className="mt-3"><Image src={Estacion}  className="mt-2 infoimg"/></Col>
+      <Col md={6} className="mt-3"><Image src={inv[0] && inv[0].get_path}  className="mt-2 infoimg"/></Col>
       <Col md={6} className="mt-3">
-          <h4 className="mt-3">Estacion Cafe</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed turpis ut libero rhoncus porta. Aenean nec ante libero. Praesent ornare ornare vestibulum. Nullam ac volutpat nunc. Integer tellus turpis, mollis ac leo at, vulputate convallis lorem. Cras accumsan, arcu id dapibus volutpat, sapien odio laoreet metus, eu tempus purus tellus et tortor. Suspendisse libero arcu, luctus id enim in, placerat vestibulum ex.</p>
+          <h4 className="mt-3">{inv[0] && inv[0].titulo} </h4>
+            <p>{inv[0] && inv[0].descripcion}</p>
       </Col>
       
       
