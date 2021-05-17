7b527b9f97e79685daa35ce808ba944d24c45556
root <jorge.reales4@gmail.com>
2021-05-17 15:13:37 -0500

    finales

--- src/components/widgets/producto/index.js
+++ src/components/widgets/producto/index.js
@@ -1,31 +1,41 @@
-import React, {useState} from 'react'
+import React, {useState,useContext} from 'react'
 import {Container, Image, Row, Col, Button} from 'react-bootstrap'
 import servicio from './../../../images/servicio.jpg'
+import {mycontext} from './../../../App'
 function Serviciou(props){
+    const {servicios} = useContext(mycontext);
+    const [serv] = servicios;
+    const id = props.match.params.id;
     let size = props.width 
     let sizeText = {
         "width" : "500",
     }
     return(
         <Container className="uservicio">
-            <Row className="mb-3 mt-2">
-                <Col md={12}><h1>Terapia de Pareja y Familiar</h1></Col>
-                <hr></hr>
-            </Row>
-            <Row>
-                <Col md={4}><Image src={servicio} width="400" height="400"/></Col>
+            {serv && serv.filter(sr => sr.id === id).map(ser => {
+                 <Row className="mb-3 mt-2">
+                 <Col md={12}><h1>{ser.titulo}</h1></Col>
+           
+         <hr></hr>
+                 </Row>
+            })}
+            {serv && serv.filter(sr => sr.id === id).map(ser => {
+                <Row>
+                <Col md={4}><Image src={ser.get_path} width="400" height="400"/></Col>
                 <Col md={8}>
                     <Row className="mt-2">
                         <Col><h3>Descripcion</h3></Col>
                     </Row>
                     <Row>
-                        <Col><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mauris nisl, ullamcorper a eros quis, convallis facilisis risus. Praesent in semper lorem, non pretium augue. Nullam iaculis nulla risus, feugiat finibus diam bibendum vel. Aenean facilisis sem tellus. Pellentesque sagittis, quam non pretium varius, diam neque pellentesque ipsum, porttitor pretium leo ipsum sed quam. Quisque in congue risus, vitae lobortis lacus. Morbi ultrices odio finibus libero elementum, at dictum elit tristique.</p></Col>
+                        <Col><p>{ser.descripcion}</p></Col>
                     </Row>
                     <Row className="mt-2">
                         <Col><Button id="modalButton">Solicitar cita</Button></Col>
                     </Row>
                 </Col>
             </Row>
+            })}
+          
         </Container>
     )
 }
